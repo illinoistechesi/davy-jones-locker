ef7c48b6c8cb4c7d8928ad171d4c8b697b9ded92
Vinesh Kannan <vinesh@omnipointment.com>
2017-07-20 02:10:21 -0500

    Improve upload confirmations

--- src/upload.js
+++ src/upload.js
@@ -21,24 +21,37 @@ dataInput.addEventListener('keypress', (e) => {
 			message: "Choose a Code",
 			callback: (code) => {
 				if (code && dataStr) {
-
 					let data = JSON.parse(dataStr);
-
 					let dataRef = db.ref('davy-jones-locker/' + code);
 					dataRef.once('value', (snapshot) => {
 						if (!snapshot.exists()) {
 							dataRef.set(data).then((done) => {
 								vex.dialog.alert("Data saved with code: " + code);
 							}).catch((err) => {
+								vex.dialog.alert("Something went wrong.");
 								console.log(err);
 							});
 						} else {
-							vex.dialog.alert("Code already in use.");
+							vex.dialog.confirm({
+								message: "Code already in use. Overwrite existing data?",
+								callback: (yes) => {
+									if (yes) {
+										dataRef.set(data).then((done) => {
+											vex.dialog.alert("Data saved, overwriting code: " + code);
+										}).catch((err) => {
+											vex.dialog.alert("Something went wrong.");
+											console.log(err);
+										});
+									} else {
+										vex.dialog.alert("Data was not overwritten.");
+									}
+								}
+							});
 						}
 					}).catch((err) => {
+						vex.dialog.alert("Something went wrong.");
 						console.error(err);
 					});
-
 				}
 			}
 		});
